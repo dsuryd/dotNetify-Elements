ad1aa53680164ed0fcfe9e0e8850734b93986d33
Dicky Suryadi <dsuryd@gmail.com>
2018-01-16 08:17:49 -0800

    Added font library.

--- DevApp/client/main.js
+++ DevApp/client/main.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './routes';
 import 'bootstrap/dist/css/bootstrap.css';
 import './app/styles/app.css';
+import './fonts/fa-svg-with-js.css';
+import './fonts/fontawesome';
+import './fonts/fa-solid';
 
 ReactDOM.render(
     <App />,
