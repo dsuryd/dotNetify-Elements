e9124463566e6e5607e3553b2c859e6794dd88c4
Dicky Suryadi <dsuryd@gmail.com>
2018-06-23 12:42:20 -0700

    Updated Layout System doc.

--- DevApp/client/app/views/LayoutSystem.js
+++ DevApp/client/app/views/LayoutSystem.js
@@ -1,13 +1,99 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Markdown, VMContext, withTheme } from 'dotnetify-elements';
+import { Markdown, Panel, VMContext, withTheme } from 'dotnetify-elements';
 import Expander from '../components/Expander';
 import Article from '../components/Article';
 
 const LayoutSystem = props => (
    <Article vm="LayoutSystem" id="Content">
-      <Markdown id="Content" />
+      <Markdown id="Content">
+         <DefaultPanelLayout />
+         <HorizontalPanelLayout />
+         <FlexPanelLayout />
+      </Markdown>
    </Article>
+);
+
+const Item = styled.div`
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   width: 50px;
+   height: 50px;
+   color: white;
+   font-size: x-large;
+   background: #999;
+`;
+
+const panelSource = props => `
+\`\`\`jsx
+const MyApp = _ => (
+   <Panel${props ? ' ' + props : ''}>
+      <Item>1</Item>
+      <Item>2</Item>
+      <Item>3</Item>
+   </Panel>
+);
+\`\`\``;
+
+const DefaultPanelLayout = _ => (
+   <Panel horizontal css="margin: 2rem 0">
+      <Panel flex>
+         <Panel>
+            <Item>1</Item>
+            <Item>2</Item>
+            <Item>3</Item>
+         </Panel>
+      </Panel>
+      <Panel flex>
+         <Markdown css=".prism-code { margin:0 !important }">{panelSource()}</Markdown>
+      </Panel>
+   </Panel>
+);
+
+const HorizontalPanelLayout = _ => (
+   <Panel horizontal css="margin: 2rem 0">
+      <Panel horizontal flex>
+         <Item>1</Item>
+         <Item>2</Item>
+         <Item>3</Item>
+      </Panel>
+      <Panel flex>
+         <Markdown css=".prism-code { margin:0 !important }">{panelSource('horizontal')}</Markdown>
+      </Panel>
+   </Panel>
+);
+
+const flexPanelSource = props => `
+\`\`\`jsx
+const MyApp = _ => (
+   <Panel horizontal>
+      <Panel flex>
+         <Item>1</Item>
+      </Panel>
+      <Item>2</Item>
+      <Panel flex right>
+         <Item>3</Item>
+      </Panel>
+   </Panel>
+);
+\`\`\``;
+
+const FlexPanelLayout = _ => (
+   <Panel horizontal css="margin: 2rem 0">
+      <Panel horizontal>
+         <Panel flex>
+            <Item>1</Item>
+         </Panel>
+         <Item>2</Item>
+         <Panel flex right>
+            <Item>3</Item>
+         </Panel>
+      </Panel>
+      <Panel flex>
+         <Markdown css=".prism-code { margin:0 !important }">{flexPanelSource()}</Markdown>
+      </Panel>
+   </Panel>
 );
 
 export default withTheme(LayoutSystem);
--- DevApp/server/Docs/LayoutSystem.md
+++ DevApp/server/Docs/LayoutSystem.md
@@ -1,1 +1,21 @@
 ﻿## Layout System
+
+Positioning elements on the page can be very time-consuming, and while css is very powerful, to apply it effectively comes with a high learning curve that not every developer can afford to take time to master.  That's why _dotNetify-Elements_ comes with simple layout elements to help you do the task quickly, while still leaving room for customization.
+
+#### Layout Grid
+
+Use the layout grid elements to set up your main application structure, with the options to add header, footer, and navigation sidebar.  See [LayoutGrid](layout/grid) for details.
+
+#### Panel Element
+
+A Panel element is a container, similar to the `<div>` tag, but equipped with the ability to arrange the layout of the child elements.  
+
+By default a Panel will vertically stack the child elements with a fixed gap between them:
+
+[inset]
+
+Use the various Panel's attributes to change the layout of items.  For example, adding the _horizontal_ attribute will stack the items horizontally:
+
+[inset]
+
+Panels use css flexbox under the hood, which allows you to make it fill the available space and easily control the content's alignments.  Nest the panels, and you can create all sorts of layout:
