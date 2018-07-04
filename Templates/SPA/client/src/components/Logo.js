88b66d6c66fed204691df886ac08973d1f188409
Dicky Suryadi <dsuryd@gmail.com>
2018-07-04 16:49:53 -0700

    Added logo.

---
+++ Templates/SPA/client/src/components/Logo.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Logo = styled.div`
+	display: flex;
+	align-items: center;
+	padding-left: 1rem;
+	content: url(http://dotnetify.net/content/images/dotnetify-logo.png);
+	width: 200px;
+	height: 39px;
+`;
+
+export default Logo;
--- Templates/SPA/client/src/views/App.js
+++ Templates/SPA/client/src/views/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Main, Header, Footer, Nav, Section, VMContext } from 'dotnetify-elements';
+import { Main, Header, Footer, Nav, Section, NavDrawerButton, Panel, VMContext } from 'dotnetify-elements';
+import Logo from '../components/Logo';
 
 class App extends React.Component {
-   render() {
-      return (
-         <VMContext vm="App">
-            <Main>
-               <Header />
-               <Nav />
-               <Section />
-               <Footer />
-            </Main>
-         </VMContext>
-      );
-   }
+	render() {
+		return (
+			<VMContext vm="App">
+				<Main>
+					<Header>
+						<Panel horizontal middle>
+							<Logo />
+							<NavDrawerButton />
+						</Panel>
+					</Header>
+					<Nav />
+					<Section />
+					<Footer />
+				</Main>
+			</VMContext>
+		);
+	}
 }
 
 export default App;
