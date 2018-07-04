bab0996111b061617b76d2b2386abe71d4e8ece5
Dicky Suryadi <dsuryd@gmail.com>
2018-07-04 09:35:55 -0700

    Added distribution scripts.

---
+++ DevApp/dist/README.md
@@ -0,0 +1,8 @@
+<p align="center"><img width="400px" src="http://dotnetify.net/content/images/dotnetify-logo.png"></p>
+
+## DotNetify-Elements
+Backend-ready React components for your cross-platform .NET web applications, based on [dotNetify-React](http://dotnetify.net/react).
+
+### Documentation
+
+This github project contains all the the library's source code, [documentation](https://github.com/dsuryd/dotNetify-Elements/tree/master/ViewModels/Docs), and live demo's source code.
---
+++ DevApp/webpack.dist.config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+   mode: 'production',
+   entry: {
+      'dotnetify-elements': './client/dotnetify-elements/index.js'
+   },
+   output: {
+      filename: '[name].js',
+      path: __dirname + '/dist',
+      publicPath: '/dist/'
+   },
+   resolve: {
+      modules: [ 'client', 'node_modules' ],
+      extensions: [ '.js', '.jsx', '.tsx' ]
+   },
+   module: {
+      rules: [
+         { test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ },
+         { test: /\.tsx?$/, use: 'awesome-typescript-loader?silent=true' },
+         { test: /\.css$/, use: [ MiniCssExtractPlugin.loader, 'css-loader?minimize' ] },
+         { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' },
+         { test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url-loader' }
+      ]
+   },
+   externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      'styled-components': 'styled',
+      dotnetify: 'dotnetify'
+   },
+   plugins: [ new MiniCssExtractPlugin() ]
+};
