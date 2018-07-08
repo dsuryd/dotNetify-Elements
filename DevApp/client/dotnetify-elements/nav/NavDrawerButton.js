02067414866c03334138f20ccb7666bb85581f45
Dicky Suryadi <dsuryd@gmail.com>
2018-07-07 21:27:42 -0700

    Update.

--- DevApp/client/dotnetify-elements/nav/NavDrawerButton.js
+++ DevApp/client/dotnetify-elements/nav/NavDrawerButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import * as utils from '../utils';
 
@@ -9,9 +10,9 @@ const IconContainer = styled.button`
    padding: 0.25rem 0.75rem;
    border-radius: 0.25rem;
    border: 1px solid #999;
-   visibility: hidden;
+   display: none;
    @media (max-width: 768px) {
-      visibility: visible;
+      display: block;
    }
    ${props => props.theme.NavDrawerButton};
    ${props => props.css};
@@ -24,11 +25,25 @@ const HamburgerIcon = styled.div`
    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
 `;
 
-export const NavDrawerButton = props => {
-   const handleClick = _ => utils.toggleNavDrawer();
-   return (
-      <IconContainer onClick={handleClick} {...props}>
-         <HamburgerIcon />
-      </IconContainer>
-   );
-};
+export class NavDrawerButton extends React.Component {
+   static propTypes = {
+      // Icon.
+      icon: PropTypes.object
+   };
+
+   static componentTypes = {
+      IconContainer
+   };
+
+   render() {
+      const [ IconContainer ] = utils.resolveComponents(NavDrawerButton, this.props);
+
+      const { icon, ...props } = this.props;
+      const handleClick = _ => utils.toggleNavDrawer();
+      return (
+         <IconContainer onClick={handleClick} {...props}>
+            {icon ? icon : <HamburgerIcon />}
+         </IconContainer>
+      );
+   }
+}
