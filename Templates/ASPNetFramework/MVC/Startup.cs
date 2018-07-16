d1846cd5492eb7c0e9548a46dd4a3fb375edf614
Dicky Suryadi <dsuryd@gmail.com>
2018-07-15 20:47:27 -0700

    ASP.NET MVC Demo.

---
+++ Templates/ASPNetFramework/MVC/Startup.cs
@@ -0,0 +1,17 @@
+﻿using DotNetify;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MVC.Startup))]
+
+namespace MVC
+{
+   public class Startup
+   {
+      public void Configuration(IAppBuilder app)
+      {
+         app.Map("/signalr", map => map.RunSignalR());
+         app.UseDotNetify(config => config.RegisterAssembly(GetType().Assembly));
+      }
+   }
+}
