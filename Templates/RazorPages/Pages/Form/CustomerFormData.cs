bc935d180dc57276a1f0450edfd8aa174fa5eb8c
Dicky Suryadi <dsuryd@gmail.com>
2018-08-01 16:28:43 -0700

    Add files via upload

---
+++ Templates/RazorPages/Pages/Form/CustomerFormData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Razor.Pages.Form
+{
+    using StringDictionary = Dictionary<string, string>;
+
+    public class CustomerFormData
+    {
+        public StringDictionary Person { get; set; }
+        public StringDictionary Phone { get; set; }
+        public StringDictionary Address { get; set; }
+    }
+}
