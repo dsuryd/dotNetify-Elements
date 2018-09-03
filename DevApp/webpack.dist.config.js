0ed1cc35cbb9ae245c9af89f8bfb8bd503d5d71c
Dicky Suryadi <dsuryd@gmail.com>
2018-09-03 15:18:31 -0700

    Update webpack config.

--- DevApp/webpack.dist.config.js
+++ DevApp/webpack.dist.config.js
@@ -2,21 +2,7 @@
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-   mode: 'production',
-   entry: {
-      'dotnetify-elements': './src/dotnetify-elements/index.js'
-   },
-   output: {
-      path: __dirname + '/dist',
-      filename: '[name].js',
-      library: 'dotNetifyElements',
-      libraryTarget: 'umd'
-   },
-   resolve: {
-      modules: [ 'src', 'node_modules' ],
-      extensions: [ '.js', '.jsx', '.tsx' ]
-   },
+const baseExport = {
    module: {
       rules: [
          { test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ },
@@ -26,24 +12,69 @@ module.exports = {
          { test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url-loader' }
       ]
    },
-   externals: [
-      'bootstrap',
-      'chart.js',
-      'dotnetify',
-      'marked',
-      'moment',
-      'prismjs',
-      'prop-types',
-      'react',
-      'react-chartjs-2',
-      'react-data-grid',
-      'react-dom',
-      'react-widgets',
-      'react-widgets-moment',
-      'reactstrap',
-      'styled-components',
-      'text-mask-addons',
-      'text-mask-core'
-   ],
    plugins: [ new MiniCssExtractPlugin() ]
 };
+
+module.exports = [
+   {
+      mode: 'production',
+      entry: {
+         'dotnetify-elements': './src/dotnetify-elements/index.js'
+      },
+      output: {
+         path: __dirname + '/dist',
+         filename: '[name].js',
+         library: 'dotNetifyElements',
+         libraryTarget: 'umd'
+      },
+      resolve: {
+         modules: [ 'src', 'node_modules' ],
+         extensions: [ '.js', '.jsx', '.tsx' ]
+      },
+      externals: [
+         'bootstrap',
+         'chart.js',
+         'dotnetify',
+         'marked',
+         'moment',
+         'prismjs',
+         'prop-types',
+         'react',
+         'react-chartjs-2',
+         'react-data-grid',
+         'react-dom',
+         'react-widgets',
+         'react-widgets-moment',
+         'reactstrap',
+         'styled-components',
+         'text-mask-addons',
+         'text-mask-core'
+      ],
+      module: baseExport.module,
+      plugins: baseExport.plugins
+   },
+   {
+      mode: 'production',
+      entry: {
+         'dotnetify-elements': './src/dotnetify-elements/index.js'
+      },
+      output: {
+         path: __dirname + '/dist',
+         filename: 'lib/[name].bundle.js',
+         library: 'dotNetifyElements',
+         libraryTarget: 'umd'
+      },
+      resolve: {
+         modules: [ 'src', 'node_modules' ],
+         extensions: [ '.js', '.jsx', '.tsx' ]
+      },
+      externals: {
+         dotnetify: 'dotnetify',
+         react: 'React',
+         'react-dom': 'ReactDOM',
+         'styled-components': 'styled'
+      },
+      module: baseExport.module,
+      plugins: baseExport.plugins
+   }
+];
