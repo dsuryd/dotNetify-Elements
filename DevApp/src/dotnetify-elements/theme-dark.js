65f30e8f53cb4b758b13afd924f59d16cb7601f7
Dicky Suryadi <dsuryd@gmail.com>
2018-09-02 19:07:42 -0700

    Updated MarkdownTOC theme.

--- DevApp/src/dotnetify-elements/theme-dark.js
+++ DevApp/src/dotnetify-elements/theme-dark.js
@@ -1,9 +1,9 @@
 import lightTheme from './theme-light';
 
 const displayTheme = {
-	Alert: ``,
-	DataGrid: {
-		Container: `
+   Alert: ``,
+   DataGrid: {
+      Container: `
          .react-grid-Main {
             outline: none;
          }
@@ -14,13 +14,13 @@ const displayTheme = {
             }
          }
       `,
-		HeaderCell: `
+      HeaderCell: `
          background: #444;
       `,
-		Cell: `
+      Cell: `
          background: #303030;
       `,
-		Row: `
+      Row: `
          &.row-selected .react-grid-Cell {
             background: #1a1a1a;
          }
@@ -30,70 +30,71 @@ const displayTheme = {
             }
          }
       `
-	},
-	Label: {
-		Container: ``,
-		IconContainer: ``,
-		IconComponent: ``
-	},
-	Markdown: ``,
-	MarkdownTOC: {
-		Selected: `
+   },
+   Label: {
+      Container: ``,
+      IconContainer: ``,
+      IconComponent: ``
+   },
+   Markdown: ``,
+   MarkdownTOC: {
+      Selected: `
          font-weight: 600;
          border-bottom: 1px dashed #ddd;
       `,
-		Container: `
+      Container: `
          .toc-h1, 
          .toc-h2 {
             font-size: 1.1rem;
          }
          .toc-h3, 
-         .toc-h4 {
+         .toc-h4,
+         .toc-h5 {
             font-size: .9rem;
          } 
       `
-	}
+   }
 };
 
 const formTheme = {
-	Button: `
+   Button: `
       margin-left: 3px;
    `,
-	Checkbox: {
-		GroupContainer: ``,
-		Container: {
-			Default: ``,
-			Checked: ``
-		},
-		Label: ``,
-		Input: ``
-	},
-	Input: ``,
-	InputValidationError: `
+   Checkbox: {
+      GroupContainer: ``,
+      Container: {
+         Default: ``,
+         Checked: ``
+      },
+      Label: ``,
+      Input: ``
+   },
+   Input: ``,
+   InputValidationError: `
         border-color: red;
     `,
-	Radio: {
-		GroupContainer: ``,
-		Container: {
-			Default: ``,
-			Checked: ``
-		},
-		Label: ``,
-		Input: ``
-	},
-	RadioToggle: {
-		GroupContainer: ``,
-		Label: {
-			Default: ``,
-			Checked: ``
-		},
-		Input: ``
-	},
-	TextArea: ``
+   Radio: {
+      GroupContainer: ``,
+      Container: {
+         Default: ``,
+         Checked: ``
+      },
+      Label: ``,
+      Input: ``
+   },
+   RadioToggle: {
+      GroupContainer: ``,
+      Label: {
+         Default: ``,
+         Checked: ``
+      },
+      Input: ``
+   },
+   TextArea: ``
 };
 
 const layoutTheme = {
-	Main: `
+   Main: `
       ${lightTheme.Main}
       a {
          color: #848c94;
@@ -106,57 +107,57 @@ const layoutTheme = {
          }
       }     
    `,
-	Header: `
+   Header: `
       background: #303030;
       border-bottom: 1px solid #111;
       box-shadow: 0 0 5px 0 rgba(0,0,0,.2);
       z-index: 999;
    `,
-	Footer: `
+   Footer: `
       background: #303030;
       border-top: 1px solid #111;        
       z-index: 997;
    `,
-	Nav: `
+   Nav: `
         background: #222;
         border-right: 1px solid #111;
         z-index: 998;
     `,
-	Section: `
+   Section: `
         color: #fff;
         background: #222;
     `,
-	Panel: {
-		Container: ``,
-		ChildContainer: ``,
-		Gap: {
-			small: '.5rem',
-			large: '1rem'
-		},
-		Margin: {
-			small: '1em',
-			large: '1.5rem'
-		}
-	}
+   Panel: {
+      Container: ``,
+      ChildContainer: ``,
+      Gap: {
+         small: '.5rem',
+         large: '1rem'
+      },
+      Margin: {
+         small: '1em',
+         large: '1.5rem'
+      }
+   }
 };
 
 const navTheme = {
-	NavMenu: {
-		SelectedRoute: `
+   NavMenu: {
+      SelectedRoute: `
          > a > div > div {
             padding-left: .5rem;
             border-left: 2px solid tomato;
             font-weight: 600;
          }
       `,
-		RouteContainer: `
+      RouteContainer: `
          border-bottom: 1px solid #444;
          color: #868e96; 
          &:hover {
             background: #1a1a1a; 
          }
       `,
-		GroupContainer: `
+      GroupContainer: `
          border-bottom: 1px solid #444;
          color: #848c94;
          &:hover { 
@@ -164,70 +165,70 @@ const navTheme = {
             background: #1a1a1a;
          }
       `
-	}
+   }
 };
 
 const structureTheme = {
-	Card: {
-		Container: `background: #303030;`,
-		ImageContainer: ``,
-		HeaderContainer: `background: #444;`,
-		BodyContainer: ``,
-		FooterContainer: `background: #444;`
-	},
-	Cell: {
-		Container: `background: #303030;`,
-		HeaderContainer: `background: #444;`,
-		BodyContainer: ``
-	},
-	Collapsible: {
-		Container: ``,
-		HeaderContainer: ``
-	},
-	Field: {
-		Container: ``,
-		LabelContainer: `
+   Card: {
+      Container: `background: #303030;`,
+      ImageContainer: ``,
+      HeaderContainer: `background: #444;`,
+      BodyContainer: ``,
+      FooterContainer: `background: #444;`
+   },
+   Cell: {
+      Container: `background: #303030;`,
+      HeaderContainer: `background: #444;`,
+      BodyContainer: ``
+   },
+   Collapsible: {
+      Container: ``,
+      HeaderContainer: ``
+   },
+   Field: {
+      Container: ``,
+      LabelContainer: `
          font-weight: 400;
       `,
-		InputContainer: `
+      InputContainer: `
          input, select, textarea {
             font-weight: 500;
             ::-webkit-input-placeholder {
                font-weight: 400;
             }
          },`,
-		ValidationMessageContainer: `
+      ValidationMessageContainer: `
             color: red;
         `,
-		PlainTextContainer: ``,
-		PlainTextComponent: `
+      PlainTextContainer: ``,
+      PlainTextComponent: `
          color: #fff; 
          font-weight: 400;
       `
-	},
-	Modal: {
-		Container: `
+   },
+   Modal: {
+      Container: `
          .modal-content { 
             background: transparent; 
          }
       `,
-		HeaderContainer: `
+      HeaderContainer: `
          color: #fff;
          background: #303030; 
          border-bottom-color: #444;
       `,
-		BodyContainer: `
+      BodyContainer: `
       color: #fff;
       background: #303030;       
       `,
-		FooterContainer: `
+      FooterContainer: `
       color: #fff;
       background: #303030; 
       border-top-color: #444;      
       `
-	},
-	Tab: {
-		Container: `
+   },
+   Tab: {
+      Container: `
          ul {
             border-bottom-color: #444;
          }
@@ -237,19 +238,19 @@ const structureTheme = {
             border-color: #444;
          }
       `,
-		TabItemContainer: ``,
-		TabItem: ``
-	}
+      TabItemContainer: ``,
+      TabItem: ``
+   }
 };
 
 const darkTheme = {
-	...lightTheme,
-	...displayTheme,
-	...formTheme,
-	...layoutTheme,
-	...navTheme,
-	...structureTheme,
-	name: 'dark'
+   ...lightTheme,
+   ...displayTheme,
+   ...formTheme,
+   ...layoutTheme,
+   ...navTheme,
+   ...structureTheme,
+   name: 'dark'
 };
 
 export default darkTheme;
