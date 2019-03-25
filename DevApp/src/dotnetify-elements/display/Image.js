bd73a54fd95de88a9176e15e9337a8a5685bddf0
Dicky Suryadi <dsuryd@gmail.com>
2019-03-24 20:22:37 -0700

    Prevent children props being passed to img tag.

--- DevApp/src/dotnetify-elements/display/Image.js
+++ DevApp/src/dotnetify-elements/display/Image.js
@@ -23,7 +23,7 @@ export class Image extends Element {
 
    render() {
       const [ _Image ] = this.resolveComponents(Image);
-      const { fullId, src, ...props } = this.attrs;
+      const { fullId, src, children, ...props } = this.attrs;
       const _src = this.value || src;
 
       return _src && <_Image id={fullId} src={_src} {...props} />;
