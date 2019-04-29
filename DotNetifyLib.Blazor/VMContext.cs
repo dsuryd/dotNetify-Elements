518cf88186c369d558c2bca541973c00f94a0336
Dicky Suryadi <Dicky.Suryadi@corp.int>
2019-04-29 13:03:08 -0700

    Simplify API.

--- DotNetifyLib.Blazor/VMContext.cs
+++ DotNetifyLib.Blazor/VMContext.cs
@@ -9,31 +9,61 @@ namespace DotNetify.Blazor
 {
    public interface IVMContext<TState>
    {
-      event EventHandler<TState> StateChanged;
+      /// <summary>
+      /// Reference to the associated 'd-vm-context' HTML markup.
+      /// </summary>
+      ElementRef ElementRef { get; set; }
 
-      Task InitAsync(ElementRef elementRef);
+      /// <summary>
+      /// Initializes the element.
+      /// </summary>
+      /// <param name="stateChangedEventHandler">Gets called when the client receives state change from the server-side view model.</param>
+      /// <returns></returns>
+      Task InitAsync(EventHandler<TState> stateChangedEventHandler = null);
 
+      /// <summary>
+      /// Dispatches property value to server-side view model.
+      /// </summary>
+      /// <param name="propertyName">Name that matches a server-side view model property.</param>
+      /// <param name="propertyValue">Value to be dispatched.</param>
       Task DispatchAsync(string propertyName, object propertyValue = null);
    }
 
    public class VMContext<TState> : ComponentInterop, IVMContext<TState>
    {
       private ElementRef _vmContextElemRef;
+      private bool _hasElementRef;
       private bool _init;
 
-      public event EventHandler<TState> StateChanged;
+      private event EventHandler<TState> StateChanged;
+
+      public ElementRef ElementRef
+      {
+         get => _vmContextElemRef;
+         set
+         {
+            _vmContextElemRef = value;
+            _hasElementRef = true;
+         }
+      }
 
       public VMContext(IJSRuntime jsRuntime) : base(jsRuntime)
       {
       }
 
-      public Task InitAsync(ElementRef vmContextElementRef)
+      public Task InitAsync(EventHandler<TState> stateChangedEventHandler = null)
       {
-         if (_init) return Task.CompletedTask;
+         if (_init)
+            return Task.CompletedTask;
+         else if (!_hasElementRef)
+            throw new ArgumentNullException("ElementRef was not set. Make sure you assign it to the \"ref\" attribute of the \"d-vm-context\" tag.");
 
          _init = true;
-         _vmContextElemRef = vmContextElementRef;
-         return AddEventListenerAsync<TState>("onStateChange", vmContextElementRef, state => StateChanged?.Invoke(this, state));
+
+         if (stateChangedEventHandler != null)
+            StateChanged += stateChangedEventHandler;
+
+         return AddEventListenerAsync<TState>("onStateChange", ElementRef, state => StateChanged?.Invoke(this, state));
       }
 
       public Task DispatchAsync(string propertyName, object propertyValue = null)
