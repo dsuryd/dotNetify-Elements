f22de7b9d9a46fe3353c54a9bb7db1707f9ee788
Dicky Suryadi <dsuryd@gmail.com>
2019-09-02 13:17:41 -0700

    Update.

--- DevApp.ViewModels/Sandbox.cs
+++ DevApp.ViewModels/Sandbox.cs
@@ -1,4 +1,7 @@
-﻿using DotNetify;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DotNetify;
 using DotNetify.Elements;
 
 namespace dotNetify_Elements
@@ -7,4 +10,54 @@ namespace dotNetify_Elements
    {
       public string Content => "";
    }
+
+   public class TodoList : MulticastVM
+    {
+      private readonly List<Todo> _todos = new List<Todo>();
+
+      public class Todo
+      {
+          public int Id { get; set; }
+          public string Text { get; set;}
+      }
+
+      public List<Todo> Todos => _todos;
+
+      public string Todos_itemKey => nameof(Todo.Id);
+
+      public Action<string> Add => text =>
+      {
+        var todo = new Todo { Id = text.GetHashCode(), Text = text };
+        if (!_todos.Any(x => x.Id == todo.Id))
+        {
+          _todos.Add(todo);
+          this.AddList(nameof(Todos), todo);
+        }
+      };
+
+      public Action<Todo> Update => update =>
+      {
+          var todo = _todos.Find(x => x.Id == update.Id);
+          if (todo != null)
+          {
+             if (string.IsNullOrWhiteSpace(update.Text))
+               Remove(update.Id);
+             else {
+               todo.Text = update.Text;
+               this.UpdateList(nameof(Todos), todo);            
+             }
+          }
+      };
+
+      public Action<int> Remove => id =>
+      {
+          var todo = _todos.Find(x => x.Id == id);
+          if (todo != null)
+          {
+            _todos.Remove(todo);
+            this.RemoveList(nameof(Todos), id);
+          }
+      };
+      
+    }
 }
--- DevApp/client/app/views/Sandbox.js
+++ DevApp/client/app/views/Sandbox.js
@@ -2,10 +2,100 @@ import React from 'react';
 import { Markdown, withTheme } from 'dotnetify-elements';
 import Article from '../components/Article';
 
+import { Frame, Button, Checkbox, Cell, Panel, TextField } from 'dotnetify-elements';
+
+dotnetify.debug = true;
+
+const cellCss = `
+  .cell-body { padding: 0 }
+  .item { width: 100%; padding: .5rem }
+`;
+
+class TodoList extends React.Component {
+   constructor() {
+      super();
+      this.vm = dotnetify.react.connect('TodoList', this);
+      this.state = { Todos: [], newTodo: '', editTodoId: 0, editTodo: '' };
+   }
+
+   componentDidMount() {
+      setTimeout(() => this.todoRef.current.focus());
+   }
+
+   componentWillUnmount() {
+      this.vm.$destroy();
+   }
+
+   render() {
+      const { Todos, newTodo, editTodoId, editTodo } = this.state;
+
+      // Control the new todo and existing todo input elements.
+      const typeTodo = value => this.setState({ newTodo: value });
+      const typeEditTodo = value => this.setState({ editTodo: value });
+
+      // Called when focus leaves or Enter keypress on the new todo input.
+      const addTodo = (value, { changed }) => {
+         if (changed) {
+            this.vm.$dispatch({ Add: value });
+            setTimeout(() => this.setState({ newTodo: '' }));
+         }
+      };
+
+      // Called when existing todo is double-clicked.
+      const startEditTodo = id => {
+         const todo = this.state.Todos.find(x => x.Id === id);
+         this.setState({ editTodoId: id, editTodo: todo.Text });
+         setTimeout(() => {
+            this.editTodoRef.current.focus();
+            this.editTodoRef.current.select();
+         });
+      };
+
+      // Called when focus leaves or Enter keypress on the edited todo input.
+      const doneEditTodo = (id, value, changed) => {
+         this.setState({ editTodoId: 0, editTodo: '' });
+         this.todoRef.current.focus();
+         if (changed) this.vm.$dispatch({ Update: { Id: id, Text: value } });
+      };
+
+      return (
+         <Frame>
+            <h1>Todo List</h1>
+            <TextField
+               inputRef={ref => (this.todoRef = ref)}
+               value={newTodo}
+               placeholder="What do you want to do?"
+               onChange={typeTodo}
+               onDone={addTodo}
+            />
+            <div>
+               {Todos.map(todo => (
+                  <Cell key={todo.Id} css={cellCss} tabIndex="0">
+                     {todo.Id !== editTodoId ? (
+                        <div className="item" onDoubleClick={_ => startEditTodo(todo.Id)}>
+                           <Checkbox label={todo.Text} />
+                        </div>
+                     ) : (
+                        <TextField
+                           inputRef={ref => (this.editTodoRef = ref)}
+                           value={editTodo}
+                           onChange={typeEditTodo}
+                           onDone={(value, { changed }) => doneEditTodo(todo.Id, value, changed)}
+                           css="width: 100%"
+                        />
+                     )}
+                  </Cell>
+               ))}
+            </div>
+         </Frame>
+      );
+   }
+}
+
 const Sandbox = _ => (
    <Article vm="Sandbox" id="Content">
       <Markdown id="Content">
-         <BasicDemo />
+         <TodoList />
       </Markdown>
    </Article>
 );
@@ -33,4 +123,4 @@ const BasicDemo = () => (
    </d-main>
 );
 
-export default withTheme(Sandbox);
+export default withTheme(TodoList);
