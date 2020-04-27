c781a61d73033c871075cff9f01b928077b042ae
Dicky Suryadi <dsuryd@gmail.com>
2020-04-26 18:51:03 -0700

    Refactor to use hook.

--- DevApp/src/dotnetify-elements/utils/markdown.js
+++ DevApp/src/dotnetify-elements/utils/markdown.js
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import marked from 'marked';
 import Prism from 'prismjs';
 import 'prismjs/components/prism-csharp.js';
@@ -5,18 +6,15 @@ import 'prismjs/components/prism-jsx.js';
 import 'prismjs/components/prism-json.js';
 
 export default function markdown(text) {
-   return (
-      <div
-         dangerouslySetInnerHTML={{
-            __html: marked(text, {
-               highlight: (code, lang) => {
-                  var language = !lang || lang === 'html' ? 'markup' : lang;
-                  if (!Prism.languages[language]) language = 'markup';
+   const divRef = useCallback(el => {
+      el.innerHTML = marked(text, {
+         highlight: (code, lang) => {
+            let language = !lang || lang === 'html' ? 'markup' : lang;
+            if (!Prism.languages[language]) language = 'markup';
+            return `<span class='prism-code'>${Prism.highlight(code, Prism.languages[language])}</span>`;
+         }
+      });
+   }, []);
 
-                  return `<span class='prism-code'>${Prism.highlight(code, Prism.languages[language])}</span>`;
-               }
-            })
-         }}
-      />
-   );
+   return <div ref={divRef} />;
 }
