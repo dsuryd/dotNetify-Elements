b3670b2427214f7a309bb94716ed5e3ad65f8af5
Dicky Suryadi <dsuryd@gmail.com>
2020-05-12 12:18:43 -0700

    Revert using hook due to refresh issue.

--- DevApp/src/dotnetify-elements/utils/markdown.js
+++ DevApp/src/dotnetify-elements/utils/markdown.js
@@ -1,4 +1,3 @@
-import { useCallback } from 'react';
 import marked from 'marked';
 import Prism from 'prismjs';
 import 'prismjs/components/prism-csharp.js';
@@ -6,15 +5,18 @@ import 'prismjs/components/prism-jsx.js';
 import 'prismjs/components/prism-json.js';
 
 export default function markdown(text) {
-   const divRef = useCallback(el => {
-      el.innerHTML = marked(text, {
-         highlight: (code, lang) => {
-            let language = !lang || lang === 'html' ? 'markup' : lang;
-            if (!Prism.languages[language]) language = 'markup';
-            return `<span class='prism-code'>${Prism.highlight(code, Prism.languages[language])}</span>`;
-         }
-      });
-   }, []);
+   return (
+      <div
+         dangerouslySetInnerHTML={{
+            __html: marked(text, {
+               highlight: (code, lang) => {
+                  var language = !lang || lang === 'html' ? 'markup' : lang;
+                  if (!Prism.languages[language]) language = 'markup';
 
-   return <div ref={divRef} />;
+                  return `<span class='prism-code'>${Prism.highlight(code, Prism.languages[language])}</span>`;
+               }
+            })
+         }}
+      />
+   );
 }
